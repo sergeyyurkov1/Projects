7b3dfa7f1c252f3a9e21b940d950df2ce636f1c3
Sergey Yurkov <38420364+sergeyyurkov1@users.noreply.github.com>
2022-02-24 18:32:04 +0500

    0

--- ads_b.py
+++ ads_b.py
@@ -106,7 +106,7 @@ cluster_to_layer = assign("""
 app = Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     meta_tags=[{'name': 'viewport',
-        'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'
+        'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.6'
     }]
 )
 
