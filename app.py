941759ab74dbc84fbca0f47227c901183bf16303
Sergey Yurkov <38420364+sergeyyurkov1@users.noreply.github.com>
2022-02-25 18:04:14 +0500

    0

--- app.py
+++ app.py
@@ -245,4 +245,4 @@ def log_bounds(bounds, n_intervals):
     return geojson
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", debug=True)
+    app.run_server(debug=True) # host="0.0.0.0"
