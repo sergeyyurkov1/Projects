335dd465d12f363e53e7da62995f81b3519d543a
Sergey Yurkov <38420364+sergeyyurkov1@users.noreply.github.com>
2022-03-10 00:54:58 +0500

    Added home page

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ navbar = (
                         align="center",
                         className="g-0",
                     ),
-                    href="#",
+                    href="/",
                     style={"textDecoration": "none"},
                 ),
                 dbc.DropdownMenu(
@@ -202,6 +202,87 @@ content = html.Div(id="content")
 
 app.layout = html.Div([dcc.Location(id="url"), *navbar, content])  # , footer
 
+
+def get_card(src, title, desc, href, sc_href, sc_disabled):
+    if "http" in href:
+        external_link = True
+        target = "_blank"
+    else:
+        external_link = False
+        target = ""
+
+    return dbc.Card(
+        [
+            dbc.CardImg(src=src, top=True),
+            dbc.CardBody(
+                [
+                    html.H4(title, className="card-title"),
+                    html.P(
+                        desc,
+                        className="card-text",
+                    ),
+                    dbc.ButtonGroup(
+                        [
+                            dbc.Button(
+                                [
+                                    "Open",
+                                    " ",
+                                    html.I(className="bi bi-box-arrow-up-right"),
+                                ],
+                                color="secondary",
+                                external_link=external_link,
+                                target=target,
+                                outline=True,
+                                href=href,
+                            ),
+                            dbc.Button(
+                                html.I(className="bi bi-github"),
+                                href=sc_href,
+                                target="_blank",
+                                # className="text-secondary",
+                                color="primary",
+                                disabled=sc_disabled,
+                                # style={
+                                #     "textDecoration": "none",
+                                # },
+                            ),
+                        ]
+                    ),
+                ],
+                className="d-grid gap-2",
+            ),
+        ],
+        style={"width": "18rem", "padding": "10px", "margin": "40px 20px 0px 20px"},
+        className="bg-light ",
+    )
+
+
+_home = html.Div(
+    dbc.Col(
+        dbc.Row(
+            [
+                get_card(
+                    src="assets/images/01.jpg",
+                    title="ADS-B Tracker",
+                    desc="Simple flight tracker and weather radar",
+                    href="/adsb_tracker",
+                    sc_href="/",
+                    sc_disabled=True,
+                ),
+                get_card(
+                    src="https://via.placeholder.com/400x300?text=Image+coming+soon",
+                    title="Vaccination Goals",
+                    desc="Visualizes vaccination progress, past and future milestones",
+                    href="https://sy-projects-st.herokuapp.com/?app=Vaccination+Goal+Visualizer",
+                    sc_href="https://github.com/sergeyyurkov1/sy-projects-st/blob/main/apps/vaccination_goals.py",
+                    sc_disabled=False,
+                ),
+            ],
+            justify="center",
+        )
+    )
+)
+
 _404 = html.Div(
     dbc.Container(
         [
@@ -224,8 +305,10 @@ _404 = html.Div(
     Input("url", "pathname"),
 )
 def render_page_content(pathname):
-    if pathname == "/adsb_tracker" or pathname == "/":
+    if pathname == "/adsb_tracker":
         return adsb_tracker.layout, adsb_tracker.TITLE
+    elif pathname == "/":
+        return _home, "Projects"
     return _404, ""
 
 
